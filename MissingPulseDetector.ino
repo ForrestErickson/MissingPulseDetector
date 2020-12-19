83077b2de7cb783a5336f070bbabd56c5192a389
Forrest Erickson <forresterickson@comcast.net>
2020-12-18 19:45:40 -0500

    Add ISR for button press and set for time out of 3 seconds. This works.

--- MissingPulseDetector.ino
+++ MissingPulseDetector.ino
@@ -21,20 +21,51 @@
 #ifndef LED_BUILTIN
 	// change to fit your needs
 	// Use 0 or 1 to use DigiSpark AVR internal LED (depending revision, mine is 1)
-	#define LED_BUILTIN 13
+	//#define LED_BUILTIN 13
+  #define LED_BUILTIN 2  // ESP32 built in LED.
 #endif
 
-volatile bool status = 0;
+volatile bool status = 0;     // Holds LED status.
+
+// Button with interupt service
+const byte interruptPin = 25;
+volatile int interruptCounter = 0;
+int numberOfInterrupts = 0;
+
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR handleInterrupt() {
+  portENTER_CRITICAL_ISR(&mux);
+  
+//  if ((long)(micros() - last_micros) >= debouncing_time * 1000) {
+//    interruptCounter++;
+//  }
+//  last_micros = micros();
+
+  TimerLib.setInterval_us(timed_function, 3000000);   // Time out in 3 seconds
+  status = true;       // Got a pulse so safe.
+
+  portEXIT_CRITICAL_ISR(&mux);
+}// end IRAM_ATTR handleInterrupt
+
+
+
+
 
 void timed_function() {
-	status = !status;
+  status = false;       //Timed out so not safe.
+//	status = !status;
 }
 
 void setup() {
+  pinMode(interruptPin, INPUT_PULLUP);  // Button. Using an extarnal pull up instead of internal
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, FALLING);
+
 	pinMode(LED_BUILTIN, OUTPUT);
-	TimerLib.setInterval_us(timed_function, 1000000);
+	TimerLib.setInterval_us(timed_function, 3000000);
 }
 
 void loop() {
 	digitalWrite(LED_BUILTIN, status);
+  
 }
