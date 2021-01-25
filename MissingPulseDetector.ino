e6de322b04f76cb55cdefeea7cded789b4fc1ef7
Forrest Erickson <forresterickson@comcast.net>
2021-01-25 17:55:05 -0500

    Modify to detect real pulses from real garage door safety sensor and tested 20210125.

--- MissingPulseDetector.ino
+++ MissingPulseDetector.ino
@@ -1,24 +1,31 @@
-/** /file: Missing pulse detector
+/** /file: MissingPulseDetector
  *  /Author: Forrest Lee Erickson
  *  /Date: 20201218
  * /brief: For use to detect garage safety pulses. Will set a time out greater than about two pulses. An ISR on falling edge will reset timer.
  * /details:  If the timer ever times out a flag is set to show not safe (LED goes off). If the pulses return the flag is cleared indicating safe.
  * The main loop will light an LED when safe.
  * Tested with a button press at a very slow time out and works.
+ * 20210125 Remove C3 from Garage door sense so pulses come through.  Change timing to 30mS. This is tested and works.
+ * Test garage door is Chamberlan 2017 WiFi MyQ model. 
+ * License: Public Domain. This may kill you but is not guarenteed to do so.
+ * 
  */
 
 #include "Arduino.h"
 #include "uTimerLib.h"
 
-const char* PROGRAM_NAME = "MissingPulseDetector";         // This program
-const char* PROGRAM_VERSION = "V 0.0.0";                // Starting to rename stuff and wire up interface.
-const int BAUDRATE = 115200;            //Use a fast baud.
+const char* PROGRAM_NAME = "MissingPulseDetector";    // This program
+const char* PROGRAM_VERSION = "V 0.0.1";              // Working with missing pulse detection.
+const int BAUDRATE = 115200;                          //Use a fast baud.
 
 #define LED_IS_SAFE_DOOR 2  // ESP32 built in LED.
 volatile bool status = 0;     // Holds LED status.
 
-// Button with interupt service
-const byte interruptPin = 25;
+// Button with interupt service for testing / development. Note opposit polarity of door pulse.
+//const byte interruptPin = 25;
+
+// Garage Door Interupt pulse.
+const byte interruptPin = 16;
 
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
@@ -28,7 +35,8 @@ portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
  */
 void IRAM_ATTR handleInterrupt() {
   portENTER_CRITICAL_ISR(&mux);
-  TimerLib.setInterval_us(timed_function, 3000000);   // Time out in 3 seconds
+//  TimerLib.setInterval_us(timed_function, 3000000);   // Time out in 3 seconds
+  TimerLib.setInterval_us(timed_function, 30000);   // Time out in 30 mseconds
   status = true;       // Got a pulse so safe.
   portEXIT_CRITICAL_ISR(&mux);
 }// end IRAM_ATTR handleInterrupt
@@ -39,7 +47,7 @@ void IRAM_ATTR handleInterrupt() {
 void timed_function() {
   status = false;       //Timed out so not safe.
   Serial.print("Safety input: ");
-  if (digitalRead(interruptPin) == 1){
+  if (digitalRead(interruptPin) == 0){    //Optocoupler goes high on sleep, Low in interupt.
     Serial.println("Stuck high. Beam blocked.");  
   } else {
     Serial.println("Stuck low. Bad sensor or wire.");  
